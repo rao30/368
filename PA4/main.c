5f1740237e3ea044b2b8f4a86322872c21f116fb
rao30 <richie1317@gmail.com>
2019-03-24 22:43:45 -0400

    s

--- PA4/main.c
+++ PA4/main.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	if(strcmp(argv[1],"-e") == 0 && argc == 3) {
-		Node *root = create_node(-1);
+		Node *root = NULL;
 		int readStatus = 1;
 		int isBST = 1;
 		int isBalanced = 1;
